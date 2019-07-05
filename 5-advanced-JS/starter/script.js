6dbbe1764774ef3e936f0f8b4fb71725895fccb4
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-07-05 16:59:37 -0400

    study ES6 and advanced JS topics

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,127 @@
+// Function constructor
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function(){
+    console.log(2016 - this.yearOfBirth);
+}
+
+
+
+var john = new Person('John', 1990, 'teacher');
+
+var jane = new Person('Jane', 1969, 'designer');
+
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+*/
+
+/*
+// Object.create
+var personProto = {
+    calculaeAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+}
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane'},
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
+
+*/
+
+// Primitives vs Objects
+
+/*
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a, b);
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 20;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b){
+    a = 30;
+    b.city = 'San Francisco'
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
+*/
+/////////////////////////////
+// Lecture: Passing Functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn){
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calcAge(el) {
+    return 2016 - el;
+} 
+
+function isFullAge(el){
+    return el >= 18;
+}
+
+function maxHeartRate(el){
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calcAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(ages);
+console.log(fullAges)
+console.log(rates);
+
+*/
+/////////////////////////////////////////
+// Lecture: Functions Returning Functions
