1f4f1879835672523d857aec8ff73577868bc764
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-07-15 12:59:39 -0400

    add nextPlayer function, create winner functionality

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -12,27 +12,60 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
-document.getElementById('currents-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-        // 1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
-
-        //2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
-   
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `dice-${dice}.png`;
     //update the round score if the rolled number was not a 1
+    if(dice !== 1) {
+        // add score
+        roundScore += dice;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    } else {
+        //next player
+        nextPlayer();
+    }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // add current score to global score
+    scores[activePlayer] += roundScore;
+    //update UI 
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+    //check if player won the game
+    if(scores[activePlayer] >= 100){
+        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+    } else {
+        //next player
+        nextPlayer();
+    }
+    
+});
 
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+        
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
